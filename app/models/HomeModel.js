2f8203365b1ef8e5ea00905b418e52e31e26993b
lisongonathan <lisongobaita@gmail.com>
2025-07-10 01:29:26 +0100

    add template

--- app/models/HomeModel.js
+++ app/models/HomeModel.js
@@ -3,6 +3,25 @@ import Model from "./Model.js";
 export default class HomeModel extends Model {
     constructor() {
         super();
-        console.log("HomeModel initialized");
+        this.topics = [];
+        this.topic = {
+            id: null,
+            title: "",
+            description: "",
+            status: "open",
+            students: [],
+            tutor: {
+                id: null,
+                name: "",
+                email: "",
+                phone: ""
+            },
+            subtutor: {
+                id: null,
+                name: "",
+                email: "",
+                phone: ""
+            },
+        }
     }
 }
